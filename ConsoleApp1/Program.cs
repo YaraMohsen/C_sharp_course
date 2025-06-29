b71a3520c16819f70aac13f9cb3fa55190039cff
YaraMohsen <yaramohsen549@gmail.com>
2025-06-29 09:49:43 +0300

    Question1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-
+//Assignment 02
 namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            #region Question1
+
+            Console.WriteLine("enter the number");
+            String read_number= Console.ReadLine();
+
+            Console.WriteLine("You enter :"+read_number);
+            
+            #endregion
+
+
         }
     }
 }
