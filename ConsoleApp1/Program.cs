e122cc52267233f2d7d05677c82e5cc66bbec5df
YaraMohsen <yaramohsen549@gmail.com>
2025-07-02 19:37:56 +0300

    Question2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,14 +9,30 @@ namespace ConsoleApp1
 
             #region Question1
 
-            Console.WriteLine("enter the number");
-            String read_number= Console.ReadLine();
+            //Console.WriteLine("enter the number");
+            //String read_number= Console.ReadLine();
 
-            Console.WriteLine("You enter :"+read_number);
-            
+            //Console.WriteLine("You enter :"+read_number);
+
             #endregion
 
+            #region Question2
 
+            String IntStringVar = "150A";
+
+            int.TryParse(IntStringVar,out int res);
+            Console.WriteLine("The string is a valid integer: " + res);
+           
+
+            #endregion
+
+            #region Question3
+
+            //float DividToInt = 10.6f;
+            //int int_value = (int)DividToInt; //explicit casting
+            //
+            //Console.WriteLine(int_value);
+            #endregion
         }
     }
 }
